819fa4d6828e3a38ea47ac9a5ec05b71038618b5
Michael Praschl <michael.praschl@gmail.com>
2025-10-16 22:16:51 +0200

    initial

---
+++ Loom.Engine.Tests/DialogDefinitionTests.cs
@@ -0,0 +1,76 @@
+﻿using FluentAssertions;
+
+namespace Loom.Engine.Tests;
+
+public class DialogDefinitionTests
+{
+    [Fact]
+    public void Start_returns_a_dialog()
+    {
+        var dialog = new DialogDefinition();
+
+        dialog.Should().NotBeNull();
+    }
+}
+
+public class DialogTests
+{
+    private readonly Dialog _dialog;
+
+    private int _linesSent;
+    private Line? _lastLine;
+
+    public DialogTests()
+    {
+        var definition = new DialogDefinition
+        {
+            Lines =
+            {
+                new Line("One"),
+                new Line("Two"),
+                new Line("Three"),
+            }
+        };
+        _dialog = definition.StartDialog();
+
+        // NOTE: StartDialog() will not send a line!
+        // this is still the low level implementation without UI or Components
+
+        _dialog.OnLine += line =>
+        {
+            _linesSent++;
+            _lastLine = line;
+        };
+    }
+
+    [Fact]
+    public void Advance_sends_a_line()
+    {
+        _dialog.Advance();
+
+        _linesSent.Should().Be(1);
+        _lastLine.Text.Should().Be("One");
+    }
+    
+    [Fact]
+    public void Advance_sends_three_lines_in_order()
+    {
+        // one
+        _dialog.Advance();
+
+        _linesSent.Should().Be(1);
+        _lastLine.Text.Should().Be("One");
+
+        // two
+        _dialog.Advance();
+
+        _linesSent.Should().Be(2);
+        _lastLine.Text.Should().Be("Two");
+        
+        // three
+        _dialog.Advance();
+
+        _linesSent.Should().Be(3);
+        _lastLine.Text.Should().Be("Three");
+    }
+}
---
+++ Loom.Engine/Dialog.cs
@@ -0,0 +1,30 @@
+﻿namespace Loom.Engine;
+
+public class DialogDefinition
+{
+    public List<Line> Lines { get; } = [];
+
+    public Dialog StartDialog()
+    {
+        return new Dialog { Lines = Lines.ToList() };
+    }
+}
+
+public class Dialog
+{
+    public event Action<Line>? OnLine;
+    
+    private int _nextLine = 0;
+    
+    public List<Line> Lines { get; internal init; } = [];
+
+    public void Advance()
+    {
+        if (_nextLine >= Lines.Count) return;
+        
+        var line = Lines[_nextLine++];
+        OnLine?.Invoke(line);
+    }
+}
+
+public record Line(string Text);
