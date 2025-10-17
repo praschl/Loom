71201bcc85760ddebcfc6bd9813b0d15c7dd6998
Michael Praschl <michael.praschl@gmail.com>
2025-10-17 15:33:11 +0200

    cleanup and refacs

--- Loom.Engine.Tests/DialogDefinitionTests.cs
+++ Loom.Engine.Tests/DialogDefinitionTests.cs
@@ -65,24 +65,3 @@ public class DialogTests
         _lastLine.Text.Should().Be("Three");
     }
 }
-
-public static class TestData
-{
-    public static class Dialog
-    {
-        public static DialogDefinition Empty() => new DialogDefinition();
-        
-        public static DialogDefinition With3Lines()
-        {
-            return new DialogDefinition
-            {
-                Lines =
-                {
-                    new Line("One"),
-                    new Line("Two"),
-                    new Line("Three"),
-                }
-            };
-        }
-    }
-} 
---
+++ Loom.Engine.Tests/TestData.cs
@@ -0,0 +1,22 @@
+﻿namespace Loom.Engine.Tests;
+
+public static class TestData
+{
+    public static class Dialog
+    {
+        public static DialogDefinition Empty() => new DialogDefinition();
+        
+        public static DialogDefinition With3Lines()
+        {
+            return new DialogDefinition
+            {
+                Lines =
+                {
+                    new Line("One"),
+                    new Line("Two"),
+                    new Line("Three"),
+                }
+            };
+        }
+    }
+}
