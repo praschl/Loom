4f503f26ee45352d488572ad692f776990719f74
Michael Praschl <michael.praschl@gmail.com>
2025-10-21 17:50:49 +0200

    move out LoomValue

--- Loom.Engine/Node.cs
+++ Loom.Engine/Node.cs
@@ -1,7 +1,4 @@
-﻿using System.Diagnostics;
-using System.Globalization;
-
-namespace Loom.Engine;
+﻿namespace Loom.Engine;
 
 public interface IEvaluateable
 {
@@ -29,73 +26,3 @@ public abstract record ContentNode : Node
 //   dialogRunner.Advance()
 // - command was executed without knowing about async stuff, and immediately continues to first real content, displaying "here are my wares"
 // - only when the inventory closed, the next line will appear
-
-public readonly record struct LoomValue
-{
-    public enum LoomType
-    {
-        String,
-        Number, // (use decimal)
-        Boolean,
-    }
-    
-    public LoomType Type { get; }
-
-    public string StringValue { get; }
-    public decimal NumberValue { get; }
-    public bool BooleanValue { get; }
-
-    public override string ToString()
-    {
-        return Type switch
-        {
-            LoomType.String => StringValue,
-            LoomType.Number => NumberValue.ToString(CultureInfo.CurrentCulture),
-            LoomType.Boolean => BooleanValue.ToString(CultureInfo.CurrentCulture),
-            _ => throw new UnreachableException()
-        };
-    }
-
-    public bool Equals(LoomValue other)
-    {
-        if (Type != other.Type)
-            return false;
-
-        return Type switch
-        {
-            LoomType.String => StringValue == other.StringValue,
-            LoomType.Number => NumberValue == other.NumberValue,
-            LoomType.Boolean => BooleanValue == other.BooleanValue,
-            _ => throw new UnreachableException()
-        };
-    }
-
-    public override int GetHashCode()
-    {
-        return Type switch
-        {
-            LoomType.String => HashCode.Combine((int)Type, StringValue.GetHashCode()),
-            LoomType.Number => HashCode.Combine((int)Type, NumberValue.GetHashCode()),
-            LoomType.Boolean => HashCode.Combine((int)Type, BooleanValue.GetHashCode()),
-            _ => throw new UnreachableException()
-        };
-    }
-
-    public LoomValue(string stringValue)
-    {
-        Type = LoomType.String;
-        StringValue = stringValue;
-    }
-
-    public LoomValue(decimal numberValue)
-    {
-        Type = LoomType.Number;
-        NumberValue = numberValue;
-    }
-
-    public LoomValue(bool booleanValue)
-    {
-        Type = LoomType.Boolean;
-        BooleanValue = booleanValue;
-    }
-}
